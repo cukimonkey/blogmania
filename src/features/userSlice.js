fc183d1f9dbf82064b9c4c44511a34c8d5637d57
Ursula Armstrong <cukimonkey@gmail.com>
2023-06-12 18:04:56 +0100

    initial commit

---
+++ src/features/userSlice.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        isSignedIn: false,
+        userData: null,
+        searchImput: 'tech',
+        blogData: null
+        },
+
+    reducers: {
+        setSignedIn: (state, action) => {
+            state.isSignedIn = action.payload;
+        },
+        setUserData: (state, action) => {
+            state.userData = action.payload;
+        },
+        setInput: (state, action) => {
+            state.searchInput = action.payload;
+        },
+        setBlogData: (state, action) => {
+            state.blogData = action.payload;
+        },
+        },
+});
+
+export const {
+    setSignedIn,
+    setUserData,
+    setInput,
+    setBlogData,
+} = userSlice.actions;
+
+export const selectSignedIn = (state) => state.user.isSignedIn;
+export const selectUserData = (state) => state.user.userData;
+export const selectUserInpot = (state) => state.user.searchInput;
+export const selectBlogData = (state) => state.user.blogData;
+
+export default userSlice.reducer;
